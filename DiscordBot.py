15235f913163ee9b03e5320aba30ca274f6a69dd
stbam <55661516+stbam@users.noreply.github.com>
2025-06-15 23:20:18 -0400

    added checks for uploading empty post and can post multiple jobs at same time

--- DiscordBot.py
+++ DiscordBot.py
@@ -10,38 +10,35 @@ CHANNEL_ID = 1383909771702435933  # your channel ID as int
 
 bot = commands.Bot(command_prefix="$", intents=discord.Intents.default())
 
-GOOGLE_SHEET_API = "https://script.google.com/macros/s/AKfycbySlKkQgbG6uSeLXE_ftU5U4DXFNsee50_jc6QCpozLKrXLVHtL3LFIGGSpXIdosaGB/exec"
+GOOGLE_SHEET_API = "https://script.google.com/macros/s/AKfycbwoDvO04YAIWixvBIb1-rsTl_vG1_qhRLD4qUtLD8ilYB92Xr5jAiBEvFUdYXr3gPwf/exec"
                         
 
 last_job = None
-
+sent_jobs = set()
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     check_for_new_jobs.start()
 
-@tasks.loop(seconds=10) #)minutes=1
+@tasks.loop(seconds=10)
 async def check_for_new_jobs():
     global last_job
-
-    print(last_job)
+    global sent_jobs
     try:
         response = requests.get(GOOGLE_SHEET_API)
-        print("Raw job data:", response.text)
+        job_list = response.json()
+        channel = bot.get_channel(CHANNEL_ID)
+        for job_data in job_list:
+            current_job = f"{job_data['jobTitle']} - {job_data['company']}"
+            if current_job not in sent_jobs and job_data.get("jobLink"):
+                sent_jobs.add(current_job)
+                await channel.send(
+                    f"📢 New job posted!\n{current_job}\nLocation: {job_data['location']}\nLink: {job_data['jobLink']}"
+                )
 
-        job_data = response.json()
-        current_job = f"{job_data['jobTitle']} - {job_data['company']}"
-
-  
-        if current_job != last_job:
-            last_job = current_job
-            channel = bot.get_channel(CHANNEL_ID)
-            if channel:
-                await channel.send(f"📢 New job posted!\n{current_job}\nLocation: {job_data['location']}\nLink: {job_data['jobLink']}")
-            else:
-                print(f"Channel with ID {CHANNEL_ID} not found")
     except Exception as e:
         print(f"Error fetching jobs: {e}")
+
         
 
 bot.run(DISCORD_TOKEN)
